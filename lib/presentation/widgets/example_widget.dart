befc394b423fa1766d119ba8c177364feba570f8
itstor <ahmdthoriq5@gmail.com>
2022-10-08 10:47:05 +0700

    feat: add example button :rocket:

---
+++ lib/presentation/widgets/example_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExampleWidget extends StatelessWidget {
+  const ExampleWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
