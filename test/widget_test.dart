b4d9456ce0fc6a1137bb61a34b8091b48cf77aa2
itstor <ahmdthoriq5@gmail.com>
2022-10-08 21:53:11 +0700

    refactor: move `main.dart` to `lib` 🔨

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:b201_app/presentation/main.dart';
+import 'package:b201_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
