b3529dba599193db619f3ed68f16fb5c187544e6
itstor <ahmdthoriq5@gmail.com>
2022-10-09 10:20:51 +0700

    feat: add text theme 🚀

---
+++ lib/app/core/themes/text_theme.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextTheme textTheme = TextTheme(
+  headline1: GoogleFonts.poppins(
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+  ),
+  headline2: GoogleFonts.poppins(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+  ),
+  subtitle1: GoogleFonts.poppins(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  ),
+  subtitle2: GoogleFonts.poppins(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  ),
+  bodyText1: GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+  ),
+  bodyText2: GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  ),
+  button: GoogleFonts.poppins(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  ),
+  caption: GoogleFonts.poppins(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  ),
+).apply(
+  bodyColor: Colors.black,
+);
