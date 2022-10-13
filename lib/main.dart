9996dcdace77730a2b854a234aa506ce4c116fcd
itstor <ahmdthoriq5@gmail.com>
2022-10-13 22:48:41 +0700

    refactor: separate `app` and `main` 🔨

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,6 @@
+import 'package:b201_app/presentation/app.dart';
 import 'package:flutter/material.dart';
-import 'package:b201_app/app/core/app_routes.dart';
-import 'package:get/get.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      getPages: AppPages.routes,
-      initialRoute: AppRoutes.home,
-    );
-  }
+  runApp(const App());
 }
