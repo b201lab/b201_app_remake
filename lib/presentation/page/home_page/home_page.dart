9d2b9a7a16716115bbff240ae508fd804f71d735
itstor <ahmdthoriq5@gmail.com>
2022-10-15 19:46:06 +0700

    feat: change homepage scaffold 🚀

--- lib/presentation/page/home_page/home_page.dart
+++ lib/presentation/page/home_page/home_page.dart
@@ -5,6 +5,13 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: const Center(
+        child: Text('Home Page'),
+      ),
+    );
   }
 }
